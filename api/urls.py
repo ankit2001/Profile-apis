2a90136a9a611f8c2f6be6993cae7bc3d8e7e8d7
ankit2001 <ankitchoudhary202.ac@gmail.com>
2020-06-28 14:16:53 +0530

    Add profile APIS

---
+++ api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from api import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, base_name = 'hello-viewset')
+router.register('profile', views.UserProfileViewSet)
+urlpatterns = [
+    path('hello-api/', views.HelloApiView.as_view()),
+    path("", include(router.urls))
+]
---
+++ api/views.py
@@ -0,0 +1,90 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from api import serializer
+from rest_framework import status
+from rest_framework import viewsets
+from api import models
+
+class HelloApiView(APIView):
+    serializer_class = serializer.HelloSerializer
+    def get(self, request, format = None):
+        an_apiview = [
+            'APIView uses http method (Put, Patch, delete, get, post)',
+            'manualy mapped to url',
+            'gives full control',
+            'best for application'
+        ]
+        return Response({'message': "Hello", 'an_apiview': an_apiview})
+    
+    def post(self, request):
+        serializer = self.serializer_class(data = request.data)
+        
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        
+        else:
+            return Response(
+                serializer.errors,
+                status = status.HTTP_400_BAD_REQUEST,
+            )
+    
+    def delete(self, request, pk = None):
+        return Response({'method': 'DELETE'})
+    
+    def put(self, request, pk = None):
+        return Response({'method': 'PUT'})
+    
+    def patch(self, request, pk = None):
+        return Response({'method': 'PATCH'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    serializer_class = serializer.HelloSerializer
+    def list(self, request):
+        an_viewset = [
+            'APIView uses http method (Put, Patch, delete, get, post)',
+            'manualy mapped to url',
+            'gives full control',
+            'best for application'
+        ]
+        return Response({'message': "Hello", 'an_viewset': an_viewset})
+    
+    def create(self, request):
+        serializer = self.serializer_class(data = request.data)
+        
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        
+        else:
+            return Response(
+                serializer.errors,
+                status = status.HTTP_400_BAD_REQUEST,
+            )
+    
+    def retrieve(self, request, pk = None):
+        return Response({'method': 'GET'})
+    
+    def update(self, request, pk = None):
+        return Response({'method': 'PUT'})
+    
+    def partial_update(self, request, pk = None):
+        return Response({'method': 'PATCH'})
+    
+    def destroy(self, request, pk = None):
+        return Response({'method': 'DELETE'})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    serializer_class = serializer.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    # def list(self, request):
+    #     an_viewset = [
+    #         'APIView uses http method (Put, Patch, delete, get, post)',
+    #         'manualy mapped to url',
+    #         'gives full control',
+    #         'best for application'
+    #     ]
+    #     return Response({'message': "Hello", 'an_viewset': an_viewset})
